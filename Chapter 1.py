22c7a84aa989f2183b52d48afdae68faec0a3734
Nnamani StephenScripts <155476794+stephenscripts@users.noreply.github.com>
2024-01-11 19:17:08 +0100

    Python

--- Chapter 1.py
+++ Chapter 1.py
@@ -1,37 +1,49 @@
 # This is For Celsius to Fahrenheit conversion */
 import os
+
 temp = eval(input("Enter a temperature in Celsius: "))
-print("In Fahrenheit, that is: ", 9/5*temp+32)
+print("In Fahrenheit, that is: ", 9 / 5 * temp + 32)
 
-print("\n\n\n\n\n\n\n\n\n\n")
+print("\n\n\n\n\n\n")
 
-#This clears only the windows cmd
-#### ===== os.system('cls')
+# This clears only the windows cmd
+os.system('cls')
 
-# This is For finding the averave of some numbers */
+# This is For finding the average of some numbers */
 
 num1 = eval(input("Number 1: "))
 num2 = eval(input("Number 2: "))
 set_of_numbers = [num1, num2]
 
 addedNum = 3
-repeater = input("Do you want to add more Numbers? Y/N")
-if repeater == "N":
-    x = True
-else:
-    x = False
-    
-while x == False:
-    addIt = eval(input("Number: "))
-    set_of_numbers.append(addIt)
-    addedNum+1
-    repeater = input("Do you want to add more Numbers? Y/N")
-    if repeater == "N":
-        x = True
+# repeater = input("Do you want to add more Numbers? Y/N:  ")
+repeater = ""
+x = ""
+while repeater == "N" or repeater != "n":
+    repeater = input("Do you want to add more Numbers? Y/N:  ")
+    if repeater == "Y" or x == "y":
+        x = "Y"
+    elif repeater == "N" or x == "n":
+        x = "N"
     else:
-        x = False
+        e = input("Press 0 (zero) to terminate the program: ")
+        if e == "0":
+            exit(0)
+        elif e != "0":
+            continue
+
+while x == "Y":
+    print("Number", addedNum, ": ", end="")
+    addIt = eval(input())
+    set_of_numbers.append(addIt)
+    addedNum += 1
+    repeater = input("Do you want to add more Numbers? Y/N: ")
+    if repeater == "N" or repeater == "n":
+        x = "N"
+    elif repeater == "Y" or repeater == "y":
+        x = "Y"
 
 y = sum(set_of_numbers)
 z = len(set_of_numbers)
 
-print("The average of all the numbers added is: ", y/z)
+print("The average of all the numbers added is: ", y / z)
