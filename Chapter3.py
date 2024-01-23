24655bae09e314bec83cfe109ba322319ded922a
Nnamani StephenScripts <155476794+stephenscripts@users.noreply.github.com>
2024-01-23 12:05:46 +0100

    Update

---
+++ Chapter3.py
@@ -0,0 +1,14 @@
+
+groups = []
+Title = "Here are the list of Groups"
+print(Title.capitalize())
+for i in range(1, 101):
+    w = i%7
+    if w == 0:
+        w = 7
+        words = "Group: " + str(w) + "\n"
+    else:
+        words = "Group: " + str(w) + "\n"
+    groups.append(words)
+
+print(*groups)
