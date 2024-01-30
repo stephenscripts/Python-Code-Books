1efebcc4f3a1cf340577e6f6b90434c01cfc80ea
Nnamani StephenScripts <155476794+stephenscripts@users.noreply.github.com>
2024-01-30 15:18:48 +0100

    update

--- 2.5 Exercise 2.py
+++ 2.5 Exercise 2.py
@@ -33,15 +33,32 @@ for f in range(height + 1):
 # Number 15
 A_height = int(input("Please type the height of the \"A\" (only numbers): "))
 x0 = A_height
-x1 = 1
-for o in range(A_height, 0, -1):
-    if o > int(A_height / 2):
-        if o == A_height:
+x1 = []
+if A_height % 2 == 0:
+    for o in range(A_height, 0, -1):
+        if len(x1) == 0:
             print(" " * x0, "*")
             x0 -= 1
-        else:
-            print(" " * x0, "*", "" * x1, "*")
-            x1 += 1
+            x1.append("")
+        elif o == int(A_height / 2):
+            print(" " * x0, "*", " *" * int(A_height / 2))
             x0 -= 1
-    if o == int(A_height / 2):
-        print(" " * x0, "* " * A_height)
+            x1.append(" ")
+        else:
+            print(" " * x0, "*_", *x1, "*")
+            x0 -= 1
+            x1.append(" ")
+else:
+    for b in range(A_height, 0, -1):
+        if len(x1) == 0:
+            print(" " * x0, "*")
+            x0 -= 1
+            x1.append("")
+        elif b == int((A_height / 2) + 1):
+            print(" " * x0, "*_", "* " * int(A_height/2))
+            x0 -= 1
+            x1.append(" ")
+        else:
+            print(" " * x0, "*", *x1, "*")
+            x0 -= 1
+            x1.append(" ")
